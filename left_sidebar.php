2b494237e12396b87d549a1079312c4919b3f8c8
bharath140502 <86289405+bharath140502@users.noreply.github.com>
2024-04-04 10:22:33 +0530

    Add files via upload

---
+++ left_sidebar.php
@@ -0,0 +1,80 @@
+<div class="left-side-bar">
+		<div class="brand-logo">
+			<a href="index.php">
+				<img src="../vendors/images/BEPL-logo.png" alt="" class="dark-logo">
+				<img src="../vendors/images/BEPL-logo.png" alt="" class="light-logo">
+			</a>
+			<div class="close-sidebar" data-toggle="left-sidebar-close">
+				<i class="ion-close-round"></i>
+			</div>
+		</div>
+		<div class="menu-block customscroll">
+			<div class="sidebar-menu">
+				<ul id="accordion-menu">
+					<li class="dropdown">
+						<a href="index.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-house-1"></span><span class="mtext">Dashboard</span>
+						</a>
+						
+					</li>
+
+					<li class="dropdown">
+						<a href="staff.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-library"></span><span class="mtext">Manage Staff</span>
+						</a>
+						
+					</li>
+
+					<li class="dropdown">
+						<a href="attendance_view.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-library"></span><span class="mtext">Attendance</span>
+						</a>
+						
+					</li>
+					<li class="dropdown">
+						<a href="Holiday_List.php" class="dropdown-toggle no-arrow">
+							<span class="micon dw dw-library"></span><span class="mtext">View Holidays</span>
+						</a>
+						
+					</li>
+						
+					
+					<!-- <li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="micon dw dw-library"></span><span class="mtext">Staff</span>
+						</a>
+						<ul class="submenu">
+							<li><a href="add_staff.php">New Staff</a></li>
+							<li><a href="staff.php">Manage Staff</a></li>
+						</ul>
+					</li> -->
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="micon dw dw-apartment"></span><span class="mtext"> Leave </span>
+						</a>
+						<ul class="submenu">
+						    <li><a href="apply_leave.php">Apply Leave</a></li>
+							<li><a href="MY_LEAVE_HISTORY.php">My Leave History</a></li>
+							<li><a href="leaves.php">All Leave</a></li>
+							<li><a href="pending_leave.php">Pending Leave</a></li>
+							<li><a href="approved_leave.php">Approved Leave</a></li>
+							<li><a href="rejected_leave.php">Rejected Leave</a></li>
+						</ul>
+					</li>
+					<li class="dropdown">
+						<a href="javascript:;" class="dropdown-toggle">
+							<span class="micon dw dw-apartment"></span><span class="mtext"> OOD </span>
+						</a>
+						<ul class="submenu">
+						    <li><a href="apply_ood.php">Apply OOD</a></li>
+							<li><a href="my_ood_history.php">My OOD History</a></li>
+							<li><a href="ood.php">All Staff OOD</a></li>
+							<li><a href="pending_ood.php">Pending OOD</a></li>
+							<li><a href="approved_ood.php">Approved OOD</a></li>
+							<li><a href="rejected_ood.php">Rejected OOD</a></li>
+						</ul>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</div>
